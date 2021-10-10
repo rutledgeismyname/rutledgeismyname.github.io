c13773f2241e42ce2b78519c50e2f93395754075
Christopher Rutledge <56637413+rutledgeismyname@users.noreply.github.com>
2021-10-10 12:33:16 -0400

    push

--- js/script.js
+++ js/script.js
@@ -4,12 +4,13 @@
 /////// Easter Egg ////////////
 /////////////
 const element = document.querySelector(".background-fun");
+
 function rotateAnimate() {
-  document.documentElement.style.setProperty("--animate-duration", "3s");
-  element.classList.add("animate__animated", "animate__rotateOut");
-  element.addEventListener("animationend", () => {
-    element.classList.remove("animate__animated", "animate__rotateOut");
-  });
+    document.documentElement.style.setProperty("--animate-duration", "3s");
+    element.classList.add("animate__animated", "animate__rotateOut");
+    element.addEventListener("animationend", () => {
+        element.classList.remove("animate__animated", "animate__rotateOut");
+    });
 }
 //////////////
 /////// Date ////////////
@@ -23,21 +24,21 @@ document.getElementById("date").innerHTML = `${topRightDateString}`;
 /////////////
 const $temp = $("<input>");
 const $url = $(location).attr("href");
-$(".clip").on("click", function () {
-  $("body").append($temp);
-  $temp.val($url).select();
-  document.execCommand("copy");
-  $temp.remove();
+$(".clip").on("click", function() {
+    $("body").append($temp);
+    $temp.val($url).select();
+    document.execCommand("copy");
+    $temp.remove();
 });
 
 //////////////
 /////// Clipboard Alert ////////////
 /////////////
 function clipAlert() {
-  $("#clipAlert").show();
-  setTimeout(function () {
-    $("#clipAlert").hide();
-  }, 2300);
+    $("#clipAlert").show();
+    setTimeout(function() {
+        $("#clipAlert").hide();
+    }, 2300);
 }
 
 //////////////
@@ -48,60 +49,61 @@ const randomUsername = Math.trunc(Math.random() * 999) + 1;
 const nameInput = document.getElementById("name");
 
 document
-  .querySelector("form.form-signin")
-  .addEventListener("submit", function (sign) {
-    sign.preventDefault();
-    //  console.log(nameInput.value);
-    if (`${nameInput.value}` !== "") {
-      document.getElementById(
-        "username"
-      ).innerHTML = `Welcome, ${nameInput.value}`;
-    } else {
-      document.getElementById(
-        "username"
-      ).innerHTML = `Welcome, user${randomUsername}`;
-    }
-  });
+    .querySelector("form.form-signin")
+    .addEventListener("submit", function(sign) {
+        sign.preventDefault();
+        //  console.log(nameInput.value);
+        if (`${nameInput.value}` !== "") {
+            document.getElementById(
+                "username"
+            ).innerHTML = `Welcome, ${nameInput.value}`;
+        } else {
+            document.getElementById(
+                "username"
+            ).innerHTML = `Welcome, user${randomUsername}`;
+        }
+    });
 
 function hideLogin() {
-  $(".form-container").css("display", "none");
-  $(".folder-container-1").css("display", "block");
-  $(".folder-container-2").css("display", "block");
-  $(".notification").delay(12200).fadeIn();
-  $(".navbar").css("visibility", "visible");
+    $(".form-container").css("display", "none");
+    $(".folder-container-1").css("display", "block");
+    $(".folder-container-2").css("display", "block");
+    $(".notification").delay(12200).fadeIn();
+    $(".navbar").css("visibility", "visible");
 }
 
 function showLogin() {
-  $(".form-container").css("display", "block");
-  $(".folder-container-1").css("display", "none");
-  $(".folder-container-2").css("display", "none");
-  $(".notification").css("display", "none");
-  $(".navbar").css("visibility", "hidden");
+    $(".form-container").css("display", "block");
+    $(".folder-container-1").css("display", "none");
+    $(".folder-container-2").css("display", "none");
+    $(".notification").css("display", "none");
+    $(".navbar").css("visibility", "hidden");
 }
 
 function sleep() {
-  document.querySelector("body").style.visibility = "visible";
-  document.querySelector("html").style.backgroundColor = "black";
-  document.querySelector("body").style.opacity = "0";
-  document.querySelector("body").style.transition = ".3s";
+    document.querySelector("body").style.visibility = "visible";
+    document.querySelector("html").style.backgroundColor = "black";
+    document.querySelector("body").style.opacity = "0";
+    document.querySelector("body").style.transition = ".3s";
 }
+
 function sleepOff() {
-  document.querySelector("body").style.visibility = "visible";
-  document.querySelector("html").style.backgroundColor = "black";
-  document.querySelector("body").style.opacity = "1";
-  document.querySelector("body").style.transition = ".3s";
-  document.querySelector("body").style.transitionDelay = ".3s";
+    document.querySelector("body").style.visibility = "visible";
+    document.querySelector("html").style.backgroundColor = "black";
+    document.querySelector("body").style.opacity = "1";
+    document.querySelector("body").style.transition = ".3s";
+    document.querySelector("body").style.transitionDelay = ".3s";
 }
 
 // Reset Folder position on Clean Up
-$(".organizeFolder").click(function () {
-  // Reset position
-  $(".resize-drag").removeAttr("style").css("transition", ".5s");
-  $(".resize-drag").removeAttr('data-x data-y');
-  setTimeout(function () {
-    $(".resize-drag").removeAttr("style");
-  }, 600);
- });
+$(".organizeFolder").click(function() {
+    // Reset position
+    $(".resize-drag").removeAttr("style").css("transition", ".5s");
+    $(".resize-drag").removeAttr('data-x data-y');
+    setTimeout(function() {
+        $(".resize-drag").removeAttr("style");
+    }, 600);
+});
 
 //////////////
 /////// Batter Level ////////////
@@ -110,57 +112,60 @@ const batteryPromise = navigator.getBattery();
 batteryPromise.then(batteryCallback);
 
 function batteryCallback(batteryObject) {
-  printBatteryStatus(batteryObject);
+    printBatteryStatus(batteryObject);
 }
 
 function printBatteryStatus(batteryObject) {
-  if (batteryObject.level >= 0.9) {
-    document.querySelector(
-      ".battery"
-    ).innerHTML = `<i class="fa fa-battery-full">  <span>${Math.trunc(
+    if (batteryObject.level >= 0.9) {
+        document.querySelector(
+            ".battery"
+        ).innerHTML = `<i class="fa fa-battery-full">  <span>${Math.trunc(
       batteryObject.level * 100
     )}%</span>`;
-  } else if (batteryObject.level <= 0.89 >= 0.4) {
-    document.querySelector(
-      ".battery"
-    ).innerHTML = `<i class="fa fa-battery-half"> <span>${Math.trunc(
+    } else if (batteryObject.level <= 0.89 >= 0.4) {
+        document.querySelector(
+            ".battery"
+        ).innerHTML = `<i class="fa fa-battery-half"> <span>${Math.trunc(
       batteryObject.level * 100
     )}%</span>`;
-  }
-  if (batteryObject.level <= 0.25) {
-    //setTimeout(function() { alert("Looks like you are running low on battery life. You should plug in."); }, 15500);
-    document.querySelector(
-      ".battery"
-    ).innerHTML = `<i class="fa fa-battery-quarter"> <span>${Math.trunc(
+    }
+    if (batteryObject.level <= 0.25) {
+        //setTimeout(function() { alert("Looks like you are running low on battery life. You should plug in."); }, 15500);
+        document.querySelector(
+            ".battery"
+        ).innerHTML = `<i class="fa fa-battery-quarter"> <span>${Math.trunc(
       batteryObject.level * 100
     )}%</span>`;
-  }
-  
+    }
+
 }
 //////////////
 /////// Checking battery status every 5 seconds ////////////
 /////////////
 setInterval(function() {
-  batteryPromise.then(batteryCallback);
-  console.log("Checking battery status every 10 seconds");
-} ,10000)
+    batteryPromise.then(batteryCallback);
+    console.log("Checking battery status every 10 seconds");
+}, 10000)
 
 //////////////
 /////// Change Background for Users ////////////
 /////////////
 
 function bgImgNone() {
-  document.querySelector("body").style.backgroundImage =
-    "url(images/bgnone.jpg)";
+    document.querySelector("body").style.backgroundImage =
+        "url(images/bgnone.jpg)";
 }
+
 function bgSand() {
-  document.querySelector("body").style.backgroundImage = "url(images/sand-night.jpg)";
+    document.querySelector("body").style.backgroundImage = "url(images/sand-night.jpg)";
 }
+
 function bgOcean() {
-  document.querySelector("body").style.backgroundImage = "url(images/ocean.jpg)";
+    document.querySelector("body").style.backgroundImage = "url(images/ocean.jpg)";
 }
+
 function bgSur() {
-  document.querySelector("body").style.backgroundImage = "url(images/big-sur.jpg)";
+    document.querySelector("body").style.backgroundImage = "url(images/big-sur.jpg)";
 }
 
 /*
@@ -172,5 +177,5 @@ function bgPaint() {
 /////// Custom Notification Hiding ////////////
 /////////////
 function closeNotification() {
-  document.querySelector('.notification').style.display = 'none';
+    document.querySelector('.notification').style.display = 'none';
 }
